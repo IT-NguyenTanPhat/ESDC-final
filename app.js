008ad73a03a7a64bf7ae2b26e4ddd0c7c3ecc6a8
tanphat1801 <tanphat200265@gmail.com>
2022-10-26 17:22:45 +0700

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const db = require("./config/db");
 
 var indexRouter = require("./src/routes/index");
 
@@ -17,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+db.connect();
 
 app.use("/", indexRouter);
 
---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://esdc-final:123@cluster0.cxt1omk.mongodb.net/?retryWrites=true&w=majority',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+        );
+        console.log('Connect db successfully');
+    } catch (error) {
+        console.log('Fail to connect db');
+    }
+}
+
+module.exports = { connect };
